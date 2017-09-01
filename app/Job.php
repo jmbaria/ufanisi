041f033e04ad02b9bb3eb31e246711c27375544a
Jeremiah Mbaria <cel01091@localhost.localdomain>
2017-09-01 16:34:59 +0300

    remove query log on model:job

--- app/Job.php
+++ app/Job.php
@@ -8,13 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     public static function  getJobPosts(){
-        DB::connection()->enableQueryLog();
     	$jobs = DB::table('jobs as a')
 	    	->select('a.id','a.title','b.name as industry','a.start_date','a.end_date')
 	    	->leftJoin('industries as b','a.industry_id','=','b.id')
     		->get();
         $q = DB::getQueryLog();
-        print_r(end($q));
     	return $jobs; 
     }
 
