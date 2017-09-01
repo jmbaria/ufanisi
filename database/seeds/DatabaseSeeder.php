0c0dcfabde761e77b50e9e5b73f1dbe7ba2225b9
Jeremiah Mbaria <cel01091@localhost.localdomain>
2017-09-01 13:04:10 +0300

    added employer seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(IndustryTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
+        // $this->call(IndustryTableSeeder::class);
+        // $this->call(PermissionsTableSeeder::class);
+        $this->call(EmployerTableSeeder::class);
     }
 }
---
+++ database/seeds/EmployerTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EmployerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+             array('name' => 'Employer','email' => 'admin@ufanisi.co','password' => 'bcrypt("!empl0y")','remember_token' => 1)
+             ]);
+
+		DB::table('user_permissions')->insert([
+             array('user_id' => 1,'permission_id' => 1)
+             ]);        
+    }
+}
